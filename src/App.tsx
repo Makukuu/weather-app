3178f74e21665cd7f353ff2a2bd4a4150cae7561
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-03-30 16:11:59 +0200

    Connect to Open Weather API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,40 @@
-function App() {
+import { ChangeEvent, useState } from "react";
+import { optionType } from "./types";
+
+const App = (): JSX.Element => {
+  const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${
+        import.meta.env.VITE_OPEN_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    console.log(option);
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${
+        option.lon
+      }&units=metric&appid=${import.meta.env.VITE_OPEN_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setTerm(value);
+
+    if (!value) return;
+    getSearchOptions(value);
+  };
+
   return (
     <main className='h-screen w-screen flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-yellow-400'>
       <section className='w-full md:max-w-[500px]   flex flex-col text-center justify-center p-4 md:p-10 lg:p-24 h-full md:h-full lg:h-3/4 bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700'>
@@ -10,12 +46,27 @@ function App() {
           Enter below a place you want to know the weather of and select an
           option from the dropdown
         </p>
-        <div className='flex justify-center md:mt-4'>
+        <div className='relative flex md:mt-4 '>
           <input
             type='text'
-            value={"Novi Sad"}
+            value={term}
             className='px-3 py-1 rounded-l-md border-2 border-white'
+            onChange={onInputChange}
           />
+
+          <ul className='absolute left-0 top-8 bg-white rounded-b-md'>
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + "-" + index}>
+                <button
+                  className='text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer'
+                  onClick={() => onOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+
           <button className='rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer'>
             search
           </button>
@@ -23,6 +74,6 @@ function App() {
       </section>
     </main>
   );
-}
+};
 
 export default App;
