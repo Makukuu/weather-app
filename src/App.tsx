0a0200363b0709268136fb4cda1c859c56977b60
Martin Kukuruzar <martinkukuruzar@gmail.com>
2023-03-30 16:35:20 +0200

    Submit button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { optionType } from "./types";
 
 const App = (): JSX.Element => {
   const [term, setTerm] = useState<string>("");
   const [options, setOptions] = useState<[]>([]);
+  const [city, setCity] = useState<optionType | null>(null);
 
   const getSearchOptions = (value: string) => {
     fetch(
@@ -15,16 +16,24 @@ const App = (): JSX.Element => {
       .then((data) => setOptions(data));
   };
 
-  const onOptionSelect = (option: optionType) => {
-    console.log(option);
-
+  const getForecast = (city: optionType) => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${
-        option.lon
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${
+        city.lon
       }&units=metric&appid=${import.meta.env.VITE_OPEN_API_KEY}`
     )
       .then((res) => res.json())
       .then((data) => console.log(data));
+  };
+
+  const onSubmit = () => {
+    if (!city) return;
+
+    getForecast(city);
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option);
   };
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -35,9 +44,16 @@ const App = (): JSX.Element => {
     getSearchOptions(value);
   };
 
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      setOptions([]);
+    }
+  }, [city]);
+
   return (
     <main className='h-screen w-screen flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-yellow-400'>
-      <section className='w-full md:max-w-[500px]   flex flex-col text-center justify-center p-4 md:p-10 lg:p-24 h-full md:h-full lg:h-3/4 bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700'>
+      <section className='w-full md:max-w-[500px]   flex flex-col items-center text-center justify-center p-4 md:p-10 lg:p-24 h-full md:h-full lg:h-3/4 bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700'>
         {" "}
         <h1 className='text-4xl font-thin'>
           Weather <span className='font-black'>Forecast</span>
@@ -46,7 +62,7 @@ const App = (): JSX.Element => {
           Enter below a place you want to know the weather of and select an
           option from the dropdown
         </p>
-        <div className='relative flex md:mt-4 '>
+        <div className='relative  flex  md:mt-4 '>
           <input
             type='text'
             value={term}
@@ -67,7 +83,10 @@ const App = (): JSX.Element => {
             ))}
           </ul>
 
-          <button className='rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer'>
+          <button
+            className='rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer'
+            onClick={onSubmit}
+          >
             search
           </button>
         </div>
